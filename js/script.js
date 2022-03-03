e729b78665263ac012388878f27b2b197a8576f1
Charlotte S <93612273+cbsumo305@users.noreply.github.com>
2022-03-03 14:34:27 -0600

    Fixed error in div.user-info

--- js/script.js
+++ js/script.js
@@ -15,12 +15,13 @@ const getMyData = async function () {
 }
 
 const readMyData = function (data) {
-    let userInfo = document.createElement("div.user-info");
+    let userInfo = document.createElement("div");
+    userInfo.classList.add("user-info");
 
-    let str = `<figure><img alt="user avatar" src=${data.avatar_url} /></figure>`;
-    str += `<div><p><strong>Name:</strong> ${data.name}</p>`;
-    str += `<p><strong>Bio:</strong> ${data.bio}</p><p><strong>Location:</strong> ${data.location}</p>`
-    str += `<p><strong>Number of public repos:</strong> ${data.public_repos}</p></div>`
+    let str = `<figure><img alt="user avatar" src=${data.avatar_url} />
+        </figure><div><p><strong>Name:</strong> ${data.name}</p>
+        <p><strong>Bio:</strong> ${data.bio}</p><p><strong>Location:</strong> ${data.location}</p>
+        <p><strong>Number of public repos:</strong> ${data.public_repos}</p></div>`;
 
     userInfo.innerHTML = str;
     overview.append(userInfo);
