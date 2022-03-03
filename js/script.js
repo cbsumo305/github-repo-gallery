21e825456508f79fe7d794d9b014aeafe4c70d74
Charlotte S <93612273+cbsumo305@users.noreply.github.com>
2022-03-03 11:08:18 -0600

    Fetch repo data

--- js/script.js
+++ js/script.js
@@ -1,30 +1,49 @@
 const overview = document.querySelector(".overview");
 const username = "cbsumo305";
 
-const getMyRepos = async function () {
-    var link = await fetch(`https://api.github.com/users/${username}`);
-    var repos = await link.json();
+const repoList = document.querySelector(".repo-list");
 
-    //return repos;
+const getMyData = async function () {
+    const link = await fetch(`https://api.github.com/users/${username}`);
+    const myData = await link.json();
 
-    readData(repos);
+    readMyData(myData);
 
-    console.log(repos)
+//    console.log(repos)
 }
 
-const readData = function (myRepos) {
+const readMyData = function (data) {
     let userInfo = document.createElement("div.user-info");
 
-    let str = `<figure><img alt="user avatar" src=${myRepos.avatar_url} /></figure>`;
-    str += `<div><p><strong>Name:</strong> ${myRepos.name}</p>`;
-    str += `<p><strong>Bio:</strong> ${myRepos.bio}</p><p><strong>Location:</strong> ${myRepos.location}</p>`
-    str += `<p><strong>Number of public repos:</strong> ${myRepos.public_repos}</p></div>`
+    let str = `<figure><img alt="user avatar" src=${data.avatar_url} /></figure>`;
+    str += `<div><p><strong>Name:</strong> ${data.name}</p>`;
+    str += `<p><strong>Bio:</strong> ${data.bio}</p><p><strong>Location:</strong> ${data.location}</p>`
+    str += `<p><strong>Number of public repos:</strong> ${data.public_repos}</p></div>`
 
     userInfo.innerHTML = str;
     overview.append(userInfo);
 }
 
+const getMyRepos = async function () {
+    const link = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const myRepos = await link.json();
 
+    console.log(myRepos);
+    readMyRepos(myRepos);
+}
+
+const readMyRepos = function (repos) {
+    repoList.innerHTML = "";
+    for (let repo of repos) {
+        //repoList
+        let li = document.createElement("li.repo");
+        li.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(li);
+    }
+}
+
+
+getMyData();
 getMyRepos();
 
 
